844e8e01443ab46596da062144b66812235c855e
kevin-fulcrum <kevinariassistemas@gmail.com>
2020-12-19 10:13:38 -0500

    [feacture]-create gallery and maps

--- src/screens/map/Map.js
+++ src/screens/map/Map.js
@@ -1,10 +1,25 @@
-import React from 'react'
-import {Text} from 'react-native'
+import React from 'react';
+import {StyleSheet, SafeAreaView} from 'react-native';
 
-const Map = () =>{
-    return(
-        <Text>hola</Text>
-    )
-}
+import GoogleMaps from '../../components/Maps/Android/';
+
+const styles = StyleSheet.create({
+  containerSafeArea: {
+    flex: 1,
+    backgroundColor: '#f9f9f9',
+  },
+});
+
+const Map = ({navigation, route}) => {
+  const {title, image, id, price, description, category, collections} =
+    route.params || {};
+  return (
+    <>
+      <SafeAreaView style={styles.containerSafeArea}>
+        <GoogleMaps />
+      </SafeAreaView>
+    </>
+  );
+};
 
 export default Map;
